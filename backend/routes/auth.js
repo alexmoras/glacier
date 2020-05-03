f1640b6914606c42eb91e1e19e08e3364dd75995
Alexander Moras <hello@alexmoras.com>
2020-05-03 19:13:00 +0100

    Basic Auth is complete - Magic-token to JWT exchange is now complete. - Email based auth is done.
    TODO
    - Tidy up the mailer system.
    - Add SSO.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var router = express.Router();
-//const jwt = require('jsonwebtoken');
-//const config = require('../config');
-//const User = require('../models/user');
+const jwt = require('../helpers/jwt');
+const User = require('../models/user');
 const EmailToken = require('../models/email-token');
 const responder = require('../helpers/responder');
-//const payload = require('../helpers/jwt-payload');
-//const expiration = require('../helpers/expiration');
 const mailer = require('../helpers/mailer');
 
+/* GENERATE MAGIC-LINK AND EMAIL TO USER */
 router.post('/login', (req, res, next) => {
     let email = escape(req.body.email);
-    // add recaptcha
+    // TODO: add recaptcha
 
     EmailToken({ email: email }).save()
         .then(email_token => {
@@ -26,6 +24,39 @@ router.post('/login', (req, res, next) => {
                     responder.failure(res, 500, 500, "An error has occurred. Please try again later",
                         err);
                 })
+        })
+        .catch(err => {
+            responder.failure(res, 500, 500, "An error has occurred. Please try again later",
+                err);
+        });
+});
+
+/* EXCHANGE MAGIC-TOKEN FOR A JSON WEB TOKEN (JWT) */
+router.post('/token', (req, res, next) => {
+    let token = escape(req.body.token);
+    EmailToken.findById(token)
+        .then(token => {
+            if(!token){
+                responder.failure(res, 404, 404, "The provided token was invalid.");
+            } else {
+                User.findOne({"email": token.email})
+                    .then(user => {
+                        if (!user) {
+                            user = User({"email": token.email});
+                            user.save()
+                                .catch(err => {
+                                    responder.failure(res, 500, 500, "An error has occurred. Please try again later",
+                                        err);
+                                });
+                        }
+                        responder.success(res, 202, 202, {"token": jwt.generate(user)});
+                        token.remove();
+                    })
+                    .catch(err => {
+                        responder.failure(res, 500, 500, "An error has occurred. Please try again later",
+                            err);
+                    });
+            }
         })
         .catch(err => {
             responder.failure(res, 500, 500, "An error has occurred. Please try again later",
