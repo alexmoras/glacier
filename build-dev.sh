38955d1fa66fed6a9fe44f55cd67d8bfcea5e315
Alexander Moras <hello@alexmoras.com>
2020-05-25 16:36:40 +0100

    Auto deploy - final (hopefully)

--- build-dev.sh
+++ build-dev.sh
@@ -1,1 +1,1 @@
-eval "$(ssh-agent -s)" && ssh-add $TRAVIS_BUILD_DIR/ssh-key && ssh -o "StrictHostKeyChecking no" -i $TRAVIS_BUILD_DIR/ssh-key vectre@sucs.org 'ssh glacier@137.44.10.96 "sh ~/build-dev"'
+eval "$(ssh-agent -s)" && ssh-add $TRAVIS_BUILD_DIR/ssh-key && ssh -o "StrictHostKeyChecking no" -i $TRAVIS_BUILD_DIR/ssh-key vectre@sucs.org 'ssh glacier@137.44.10.96 "./build-dev.sh"'
--- build-staging.sh
+++ build-staging.sh
@@ -1,1 +1,1 @@
-eval "$(ssh-agent -s)" && ssh-add $TRAVIS_BUILD_DIR/ssh-key && ssh -o "StrictHostKeyChecking no" -i $TRAVIS_BUILD_DIR/ssh-key vectre@sucs.org 'ssh glacier@137.44.10.96 "sh ~/build-staging"'
+eval "$(ssh-agent -s)" && ssh-add $TRAVIS_BUILD_DIR/ssh-key && ssh -o "StrictHostKeyChecking no" -i $TRAVIS_BUILD_DIR/ssh-key vectre@sucs.org 'ssh glacier@137.44.10.96 "./build-staging.sh"'
