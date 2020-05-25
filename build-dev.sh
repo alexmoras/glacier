dcabb4c3af6809ca29fb3976ddc13688d8e3ad27
Alexander Moras <hello@alexmoras.com>
2020-05-25 16:25:38 +0100

    Auto deploy - updated

--- build-dev.sh
+++ build-dev.sh
@@ -1,1 +1,1 @@
-eval "$(ssh-agent -s)" && ssh-add ./ssh-key && ssh -i ./ssh-key vectre@sucs.org 'ssh glacier@137.44.10.96 "./build-dev"'
+eval "$(ssh-agent -s)" && ssh-add $TRAVIS_BUILD_DIR/ssh-key && ssh -i $TRAVIS_BUILD_DIR/ssh-key vectre@sucs.org 'ssh glacier@137.44.10.96 "./build-dev"'
