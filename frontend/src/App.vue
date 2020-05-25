5e4673ea3ddd26a230061418c89397cb02dcc8e7
Alexander Moras <hello@alexmoras.com>
2020-05-25 14:23:09 +0100

    Add alert about data security

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <b-alert class="text-center data-alert" variant="danger" show>This is a beta version. Please do not store personal details on this instance.</b-alert>
     <router-view></router-view>
   </div>
 </template>
@@ -15,5 +16,9 @@ export default {
   html, body {
     font-family: 'Roboto', sans-serif;
     font-weight: 500;
+  }
+  .data-alert{
+    margin: 0;
+    border-radius: 0;
   }
 </style>
